5d782054012483734d21b53fec5c7099a78e075b
Bilal <86686062+Bilal-A-G@users.noreply.github.com>
2025-05-08 23:16:32 -0400

    Created the DTO to send data to C++, now just need to implement the algorithm, but probably next week

---
+++ Source/Project_Urban/Public/ProceduralGeneration/TileEntryDTO.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "TileEntryDTO.generated.h"
+
+UCLASS(Blueprintable)
+class PROJECT_URBAN_API UTileEntryDTO : public UObject
+{
+	GENERATED_BODY()
+public:
+	UTileEntryDTO(){}
+public:
+	UPROPERTY(BlueprintReadWrite)
+	FString name;
+	UPROPERTY(BlueprintReadWrite)
+	UStaticMesh* mesh;
+	UPROPERTY(BlueprintReadWrite)
+	FVector rotation;
+	UPROPERTY(BlueprintReadWrite)
+	TArray<FString> upAdjacencies;
+	UPROPERTY(BlueprintReadWrite)
+	TArray<FString> downAdjacencies;
+	UPROPERTY(BlueprintReadWrite)
+	TArray<FString> leftAdjacencies;
+	UPROPERTY(BlueprintReadWrite)
+	TArray<FString> rightAdjacencies;
+
+	UPROPERTY(BlueprintReadWrite)
+	TArray<FString> allKnownTiles;
+};
